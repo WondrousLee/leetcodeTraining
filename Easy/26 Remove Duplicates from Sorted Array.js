8dcbf7e4580aed8e33b25b3cc3624857210c8a25
Marcin Cudny <55958468+BeardedShaggy@users.noreply.github.com>
2023-05-15 01:07:17 +0200

    Update 26 Remove Duplicates from Sorted Array.js

--- Easy/26 Remove Duplicates from Sorted Array.js
+++ Easy/26 Remove Duplicates from Sorted Array.js
@@ -1,7 +1,5 @@
 let nums = [1, 1, 2, 3, 3, 3]
-let status = 0;
 for (let i = 0; i < nums.length; i++){
-
       if (nums[i] == nums[i+1]){
             nums.splice(i, 1);
             i-- //oh my gawd, it was THAT easy...
