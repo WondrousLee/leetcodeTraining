fe5f7ba206cc392ebbe6374c814eea5ba534acb5
Marcin Cudny <55958468+BeardedShaggy@users.noreply.github.com>
2023-05-15 01:03:08 +0200

    Update 26 Remove Duplicates from Sorted Array.js
    it just so happens that i didn't need any "status" or other if loop...

--- Easy/26 Remove Duplicates from Sorted Array.js
+++ Easy/26 Remove Duplicates from Sorted Array.js
@@ -1,17 +1,10 @@
-
 let nums = [1, 1, 2, 3, 3, 3]
-
 let status = 0;
 for (let i = 0; i < nums.length; i++){
 
       if (nums[i] == nums[i+1]){
-        let status = 1;
-        nums.splice(i, 1);
-        if (status == 1){
-          if (nums[i] == nums[i+1]){
             nums.splice(i, 1);
-          }
-        }
+            i-- //oh my gawd, it was THAT easy...
       }
 }
 console.log(nums)
