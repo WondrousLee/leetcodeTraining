d9b673b0951fe32361c2ea78b27fb749611e8a59
Marcin Cudny <55958468+WondrousLee@users.noreply.github.com>
2023-05-17 18:16:21 +0200

    Update 58 Length of Last Word.js

--- JavaScript/Easy/58 Length of Last Word.js
+++ JavaScript/Easy/58 Length of Last Word.js
@@ -1,16 +1,18 @@
-let s = "Hello World";
+let s = "   fly me   to   the moon  ";
 
+
+let strim = s.trim() //let's trim it a little bit.
 let startOfWord; let endOfWord;
-let validValues = "asdfghjklzxcvbnmqwertyuiop-ASDFGHJKLQWERTYUIOPZXCVBNM";
-for (i = 0; i <= s.length; i++) {
+let validValues = "asdfghjklzxcvbnmqwertyuiop-ASDFGHJKLQWERTYUIOPZXCVBNM"; //might be bad to check every possible letter every time.
+for (i = 0; i <= strim.length; i++) {
 
-    if (s[i-1] == " " || s[i-1] == null) {
-        if (validValues.includes(s[i])) {
+    if (strim[i-1] == " " || strim[i-1] == null) {
+        if (validValues.includes(strim[i])) {
             startOfWord = i;
         }
     }
-    if (validValues.includes(s[i])) {
-        if (s[i + 1] == " " || s[i + 1] == null) {
+    if (validValues.includes(strim[i])) {
+        if (strim[i + 1] == " " || strim[i + 1] == null) {
             endOfWord = i
         }
     }
